c79d5de95e9cb1f67b1bf46e4a9bcad87f38ff41
Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
2014-07-01 14:44:11 -0700

    modify userspace src format

--- drivers/video/msm/mdss/mdss_mdp_rotator.c
+++ drivers/video/msm/mdss/mdss_mdp_rotator.c
@@ -570,6 +570,18 @@ int mdss_mdp_rotator_setup(struct msm_fb_data_type *mfd,
 		if (rot)
 			mdss_mdp_rotator_release(rot);
 	}
+
+
+	/*
+	 * overwrite the src format for rotator to dst format
+	 * for use by the user. On subsequent set calls, the
+	 * user is expected to proivde the original src format
+	 */
+	req->src.format = mdss_mdp_get_rotator_dst_format(req->src.format,
+		req->flags & MDP_ROT_90);
+
+	mutex_unlock(&rotator_lock);
+
 	return ret;
 }
 
